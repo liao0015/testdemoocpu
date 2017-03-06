bd2d6538b85c45c21c2ad0c5d20d00b78c31af7c
liao0015 <liao0015@algonquinlive.com>
2017-03-06 16:16:53 -0500

    web components added
    html, css and js

---
+++ inst/www/main.js
@@ -0,0 +1,46 @@
+
+
+$(document).ready(function(){
+	$("#norm-btn").click(function(){
+		ocpu.seturl("http://192.168.88.128/ocpu/library/stats/R")
+		var req = ocpu.call(
+			"rnorm", 
+			{n: 100},
+			function(session){
+				$("#key").text(session.getKey());
+				$("#location").text(session.getLoc());
+
+				session.getConsole(function(output){
+					$("#output").text(output);
+				});
+
+				session.getObject(function(data){
+					console.log('Array of length' + data.length + ".\nFirst few values: " + data.slice(0, 10));
+				});
+
+			});
+	});
+
+	$("#file-btn").click(function(){
+		ocpu.seturl("http://192.168.88.128/ocpu/library/utils/R")
+		var myfile = $("#input-file")[0].files[0];
+		var myheader = true;
+		var req = ocpu.call(
+			"read.csv",
+			{
+				"file": myfile,
+				"header": myheader 
+			},
+			function(session){
+				console.log(session);
+				session.getConsole(function(file){
+					console.log(file);
+				});
+				session.getObject(function(path){
+					console.log(path);
+				});
+				//console.log(session.getFile());
+			});
+	});
+	
+});
